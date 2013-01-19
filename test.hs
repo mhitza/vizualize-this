9c9cd9d555e889779f58ccdcacadd30a0227d821
Marius Ghita <mhitza@gmail.com>
2013-01-19 17:13:03 +0200

    Change sample rate
    Sample rate must be directly proportial with reading, otherwise
    we can consume the samples faster than they are buffered causing
    blocking waits.

--- test.hs
+++ test.hs
@@ -17,21 +17,21 @@ main = do
   isWindowOpened <- openWindow (Size 800 400) [] Window
   windowTitle $= "Visualize this"
   pointSize $= 2
-  soundSource <- P.simpleNew Nothing "X" P.Record Nothing "visualizer" (P.SampleSpec (P.F32 P.LittleEndian) 1024 2) Nothing Nothing
+  soundSource <- P.simpleNew Nothing "X" P.Record Nothing "visualizer" (P.SampleSpec (P.F32 P.LittleEndian) 4096 2) Nothing Nothing
   sampleLoop soundSource 1
 
 sampleLoop source count = do
   let additiveIterations = 1
   when (count == additiveIterations) $ do clear [ColorBuffer]
-  _ <- (P.simpleRead source $ 200 :: IO [GLfloat]) >>= renderS >> return ()
+  _ <- (P.simpleRead source $ 800 :: IO [GLfloat]) >>= renderS >> return ()
   GLFW.swapBuffers
   sampleLoop source (if count == additiveIterations then 1 else count + 1)
 
 
-rangeX = [-100..0]++[1..100]
+rangeX = [-400..0]++[1..400]
 
 renderS sampleS = do 
-  renderPrimitive Quads $ forM (zip sampleS (rangeX)) $ \(sample,p) -> do
+  renderPrimitive Triangles $ forM (zip sampleS (rangeX)) $ \(sample,p) -> do
     colorize (sin sample) (p/200) 0.5
     vertify (p/200) (sin sample) 0.0
   return ()
