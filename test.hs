c493d82c2e09dbb13d172af6f6b7c945a544b28c
Marius Ghita <mhitza@gmail.com>
2013-01-20 20:26:51 +0200

    Visual shaders

--- test.hs
+++ test.hs
@@ -1,6 +1,7 @@
 import Graphics.Rendering.OpenGL
 import Graphics.Rendering.OpenGL.GL.Colors
 import Graphics.UI.GLFW as GLFW
+import Graphics.GLUtil.Shaders as S
 
 import Sound.Pulse.Simple as P
 
@@ -17,21 +18,34 @@ main = do
   isWindowOpened <- openWindow (Size 800 400) [] Window
   windowTitle $= "Visualize this"
   pointSize $= 2
+  pointSmooth $= Enabled
   soundSource <- P.simpleNew Nothing "X" P.Record Nothing "visualizer" (P.SampleSpec (P.F32 P.LittleEndian) 4096 2) Nothing Nothing
-  sampleLoop soundSource 1
 
-sampleLoop source count = do
+  -- shading
+  vs <- S.loadShader "vshader.s"
+  fs <- S.loadShader "fshader.s"
+  p <- linkShaderProgram [vs] [fs]
+  currentProgram $= (Just p)
+
+  uL <- get (uniformLocation p "A")
+  uniform uL $= Index1 (1.0::GLfloat)
+
+  sampleLoop soundSource 1 uL
+
+sampleLoop source count ul = do
   let additiveIterations = 1
   when (count == additiveIterations) $ do clear [ColorBuffer]
-  _ <- (P.simpleRead source $ 800 :: IO [GLfloat]) >>= renderS >> return ()
+  _ <- (P.simpleRead source $ 100 :: IO [GLfloat]) >>= renderS ul >> return ()
   GLFW.swapBuffers
-  sampleLoop source (if count == additiveIterations then 1 else count + 1)
+  sampleLoop source (if count == additiveIterations then 1 else count + 1) ul
 
 
-rangeX = [-400..0]++[1..400]
+rangeX = [-50..0]++[1..50]
 
-renderS sampleS = do 
-  renderPrimitive Triangles $ forM (zip sampleS (rangeX)) $ \(sample,p) -> do
-    colorize (sin sample) (p/200) 0.5
-    vertify (p/200) (sin sample) 0.0
+renderS ul sampleS = do 
+  let max' = foldl max 0 sampleS
+  uniform ul $= Index1 (max' :: GLfloat)
+  renderPrimitive LineStrip $ forM (zip sampleS (rangeX)) $ \(sample,p) -> do
+    colorize (sin sample) (p/100) 0.5
+    vertify (p/100) (sample) 0.0
   return ()
