787064d2f791e071200844adbedad5480781e611
Marius Ghita <mhitza@gmail.com>
2013-01-21 23:09:45 +0200

    Some more complex structure rendering

--- test.hs
+++ test.hs
@@ -5,6 +5,7 @@ import Graphics.GLUtil.Shaders as S
 
 import Sound.Pulse.Simple as P
 
+import Data.Time.Clock.POSIX
 import Control.Monad
 import System.Random
 import Data.List
@@ -35,7 +36,7 @@ sampleLoop source count t = do
   let total = t + 1
   let additiveIterations = 1
   when (count == additiveIterations) $ do clear [ColorBuffer]
-  _ <- (P.simpleRead source $ sampleRate :: IO [GLfloat]) >>= renderS >> return ()
+  _ <- (P.simpleRead source $ sampleRate :: IO [GLfloat]) >>= renderS total >> return ()
   GLFW.swapBuffers
   sampleLoop source (if count == additiveIterations then 1 else count + 1)  total
 
@@ -60,11 +61,11 @@ squareDrawR (w, h, x, y) samples = do
 
     let positions = concatMap
           (\d -> [((x - w/d), (y + h/d)),((x - w/d),(y - h/d)),((x + w/d),(y - h/d)),((x + w/d),(y + h/d))])
-          $ iterateF 
+          $ iterateP 2 4
     forM_ (zip samples (iterate (+1) 2)) $ \(sample@(_,r), div) -> do
-      -- rGen <- newStdGen
-      -- let positions' = nub . map (positions !!) . take 20 $ randomRs (0,40) rGen
-      mapM_ (\(x,y) -> drawSquare ((w/div, h/div, x, y)) sample) $ take 40 positions
+      rGen <- newStdGen
+      let positions' = nub . map (positions !!) . take 20 $ randomRs (0,40) rGen
+      mapM_ (\(x,y) -> drawSquare ((w/div, h/div, x, y)) sample) $ take 20 positions
 
 continuationDraw _ _ _ [] = return ()
 continuationDraw (x,y) d m (sample:samples) =
@@ -79,8 +80,16 @@ continuationDraw (x,y) d m (sample:samples) =
     let d' | d == 3 = 1 | otherwise = d + 1
     continuationDraw (x', y') d' m' samples
   
-renderS sampleS = do 
-  -- rGen <- newStdGen
-  -- renderPrimitive LineStrip $ squareDrawR (2.0, 2.0, 0.0, 0.0) (zip sampleS (randomRs (0,100) rGen))
-  renderPrimitive Lines $ continuationDraw (0.0, 0.0) 1 0.0 sampleS
+renderS count sampleS = do 
+  _ <- get currentProgram >>= \p -> case p of
+    Nothing -> return ()
+    Just p  -> do tl <- get (uniformLocation p "time")
+                  time <- getPOSIXTime >>= (return . fromIntegral . floor)
+                  uniform tl $= Index1 (time :: GLfloat)
+                  cl <- get (uniformLocation p "count")
+                  uniform cl $= Index1 (count :: GLfloat)
+                  return ()
+  rGen <- newStdGen
+  renderPrimitive Quads $ squareDrawR (2.0, 2.0, 0.0, 0.0) (zip sampleS (randomRs (0,100) rGen))
+  --renderPrimitive LineStrip $ continuationDraw (0.0, 0.0) 1 0.0 sampleS
   return ()
